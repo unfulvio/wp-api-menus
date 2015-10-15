6441e0403e3a25d7827e16016f549239545d18a8
Paul Hendrick <paul@errorstudio.co.uk>
2015-10-15 13:50:07 +0100

    split api compatibility into separate classes for wp JSON api and REST apis

--- plugin.php
+++ plugin.php
@@ -5,11 +5,11 @@
  * @author            Fulvio Notarstefano <fulvio.notarstefano@gmail.com>
  *
  * @wordpress-plugin
- * Plugin Name: WP REST API Menu routes
+ * Plugin Name: JSON REST API Menu routes
  * Description: Extends WP API with WordPress menu routes.
  * Author: Fulvio Notarstefano
  * Author URI: https://github.com/nekojira
- * Version: 1.1.6
+ * Version: 1.1.5
  * Plugin URI: https://github.com/nekojira/wp-api-menus
  */
 
@@ -37,14 +37,27 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 // include main class
-include_once dirname( __FILE__ ) . '/lib/wp-api-menus.php';
+include_once dirname( __FILE__ ) . '/lib/wp-api-menus-v1.php';
+include_once dirname( __FILE__ ) . '/lib/wp-api-menus-v2.php';
 
-if ( ! function_exists ( 'wp_rest_menus_init' ) ) :
+if ( ! function_exists ( 'wp_json_menus_init' ) ) :
 
 	/**
-	 * Init REST API Menu routes
+	 * Init JSON REST API Menu routes
 	 */
-    $class = new WP_REST_Menus();
-	add_action('rest_api_init', array($class, 'register_routes'));
+
+    function wp_rest_menus_init() {
+        $apiVersion = get_option( 'rest_api_plugin_version', get_option( 'json_api_plugin_version', null ) );
+
+        if(preg_match('/^2/', $apiVersion, $match, PREG_OFFSET_CAPTURE)){
+            $class = new WP_REST_Menus();
+            add_filter( 'rest_api_init', array( $class, 'register_routes') );
+        }else {
+            $class = new WP_JSON_Menus();
+            add_action( 'wp_json_server_before_serve', array( $class, 'register_routes' ) );
+        }
+    }
+
+    add_action( 'init', 'wp_rest_menus_init' );
 
 endif;
