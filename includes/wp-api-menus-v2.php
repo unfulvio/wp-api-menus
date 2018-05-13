8db1ad02d7cdc2b7e38ef65b943f3fbdf62e9471
Victor Decroix <victor.decroix@live.com>
2018-05-13 21:37:30 +0800

    Add get_slug by item type

--- includes/wp-api-menus-v2.php
+++ includes/wp-api-menus-v2.php
@@ -356,7 +356,6 @@ if ( ! class_exists( 'WP_REST_Menus' ) ) :
             return $nav_menu_item_list;
         }
 
-
         /**
          * Format a menu item for REST API consumption.
          *
@@ -369,6 +368,21 @@ if ( ! class_exists( 'WP_REST_Menus' ) ) :
         public function format_menu_item( $menu_item, $children = false, $menu = array() ) {
 
             $item = (array) $menu_item;
+
+            //categories, tags, etc ...
+            if($item['type'] === 'taxonomy')
+            {
+                $object_slug  = get_term($item['object_id'], $item['object'])->slug;
+            }
+            //archive
+            else if($item['type'] === 'post_type_archive'){
+                $object_slug = get_post_type_object( $item['object'] )->rewrite['slug'];
+            }
+            else{
+            // post_type, page, etc ...
+                $object_slug = get_post( $item['object_id'] )->post_name;
+            }
+
 
             $menu_item = array(
                 'id'          => abs( $item['ID'] ),
@@ -383,7 +397,7 @@ if ( ! class_exists( 'WP_REST_Menus' ) ) :
                 'description' => $item['description'],
                 'object_id'   => abs( $item['object_id'] ),
                 'object'      => $item['object'],
-                'object_slug' => get_post( $item['object_id'] )->post_name,
+                'object_slug' => $object_slug,
                 'type'        => $item['type'],
                 'type_label'  => $item['type_label'],
             );
@@ -394,6 +408,8 @@ if ( ! class_exists( 'WP_REST_Menus' ) ) :
 
             return apply_filters( 'rest_menus_format_menu_item', $menu_item );
         }
+
+         
 
 
     }
