f93ca5b613e4dee26ddfd9a761d7ded722192f5a
Aurovrata V <vrata@syllogic.in>
2018-05-31 22:04:03 +0530

    fixed rest base name

--- includes/wp-api-menus-v2.php
+++ includes/wp-api-menus-v2.php
@@ -365,21 +365,38 @@ if ( ! class_exists( 'WP_REST_Menus' ) ) :
          * @return array	a formatted menu item for REST
          */
         public function format_menu_item( $menu_item, $children = false, $menu = array() ) {
-
             $item = (array) $menu_item;
             $object_slug=null;
+            $rest_path = '';
             switch($item['type']){
                 case 'post_type_archive': //archive.
                     $post_type_data = get_post_type_object( $item['object'] );
+                    $object = empty($post_type_data->rest_base) ? $item['object'] : $post_type_data->rest_base;
                     $object_slug = $post_type_data->rewrite['slug'];
+
+                    $rest_path = rest_url( '/wp/v2/'.$object.'/');
                     break;
                 case 'taxonomy'://taxonomy term.
                     $term = get_term_by( 'id', $item['object_id'], $item['object']);
                     $object_slug = $term->slug;
+                    $taxonomy = get_taxonomy( $item['object']);
+                    $object =  ! empty( $taxonomy->rest_base ) ? $taxonomy->rest_base : $taxonomy->name;
+                    
+                    $rest_path = rest_url( '/wp/v2/'.$object.'/'.$item['object_id']);
+                    break;
                 case 'post_type':
+                    $post_type_data = get_post_type_object( $item['object'] );
+                    $object = empty($post_type_data->rest_base) ? $item['object'] : $post_type_data->rest_base;
                     $object_slug = get_post( $item['object_id'] )->post_name;
+
+                    $rest_path = rest_url( '/wp/v2/'.$object.'/'.$item['object_id']);
+                    break;
+                default: //try with the object and id.
+                    $rest_path = rest_url( '/wp/v2/'.$item['object'].'/'.$item['object_id']);
                     break;
             }
+            //get_rest_url( int $blog_id = null, string $path = '/', string $scheme = 'rest' )
+
             $menu_item = array(
                 'id'          => abs( $item['ID'] ),
                 'order'       => (int) $item['menu_order'],
@@ -396,6 +413,7 @@ if ( ! class_exists( 'WP_REST_Menus' ) ) :
                 'object_slug' => $object_slug,
                 'type'        => $item['type'],
                 'type_label'  => $item['type_label'],
+                '_links'      => array('self'=>$rest_path)
             );
 
             if ( $children === true && ! empty( $menu ) ) {
@@ -410,4 +428,3 @@ if ( ! class_exists( 'WP_REST_Menus' ) ) :
 
 
 endif;
-
