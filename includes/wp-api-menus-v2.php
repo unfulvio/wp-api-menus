f161c3212642d9451bae2e29778342decc5b9949
Christian Nikkanen <me@kisu.li>
2022-02-28 22:19:38 +0200

    Fix rare instance of a PHP notice being thrown (#51)
    * Fix rare instance of a PHP notice being thrown
    
    PHP Notice:  Trying to get property of non-object in /data/wordpress/htdocs/wp-content/plugins/wp-api-menus/includes/wp-api-menus-v2.php on line 387
    
    It returns `null` in the offending item when displayed in the REST response.
    
    * Update includes/wp-api-menus-v2.php
    
    Update indention formatting
    
    * Update includes/wp-api-menus-v2.php
    
    Co-authored-by: Austin Passy <367897+thefrosty@users.noreply.github.com>

--- includes/wp-api-menus-v2.php
+++ includes/wp-api-menus-v2.php
@@ -373,6 +373,8 @@ if ( ! class_exists( 'WP_REST_Menus' ) ) :
         public function format_menu_item( $menu_item, $children = false, $menu = array() ) {
 
             $item = (array) $menu_item;
+            $object_post = get_post( $item['object_id'] );
+
 
             $menu_item = array(
                 'id'          => abs( $item['ID'] ),
@@ -387,7 +389,7 @@ if ( ! class_exists( 'WP_REST_Menus' ) ) :
                 'description' => $item['description'],
                 'object_id'   => abs( $item['object_id'] ),
                 'object'      => $item['object'],
-                'object_slug' => get_post( $item['object_id'] )->post_name,
+                'object_slug' => !empty($object_post) ? $object_post->post_name : null,
                 'type'        => $item['type'],
                 'type_label'  => $item['type_label'],
             );
